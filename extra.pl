c0d87937bc3afb6c1047a52a716a8ec26cde5dad
Oufattole <noufattole002@rsdmo.org>
2017-10-15 03:07:38 +0000

    Add files via upload

---
+++ extra.pl
@@ -0,0 +1,15 @@
+get '/logs' => sub {
+	my $c = shift;
+	
+	$c->render(template => '');
+};
+get '/user' => sub {
+	my $c = shift;
+	
+	$c->render(template => '');
+};
+get '/secret' => sub {
+	my $c = shift;
+	
+	$c->render(template => '');
+};
